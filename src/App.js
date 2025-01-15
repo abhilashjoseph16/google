5ab695866483cc3380c721d11eb786d909fbfe6c
Abhilash Joseph <abhilash.pannikottil@gmail.com>
2025-01-16 01:26:09 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import GoogleSearchBox from "./components/GoogleSearchBox";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <GoogleSearchBox/>
     </div>
   );
 }
